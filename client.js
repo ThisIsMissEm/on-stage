bba44ee9f823aad9868eef8ea0b2fb67c6d0958f
Micheil Smith <micheil@brandedcode.com>
2014-12-14 06:24:25 +0000

    WIP: Mixer Client

---
+++ client.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var net = require('net');
+var LineStream = require('byline').LineStream;
+
+// 1. Connect to liquid soap over socket
+var socket = net.createConnection({
+  path: path.resolve(__dirname, process.argv[2])
+});
+
+
+var parser = new LineStream();
+var generator = new LineStream();
+
+socket.pipe(parser);
+generator.pipe(socket);
+
+generator.write('count');
